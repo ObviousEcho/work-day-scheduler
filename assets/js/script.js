513786f404d3aa7ce30627887adc042710a2526c
Adia & Indy <107782632+ObviousEcho@users.noreply.github.com>
2022-08-14 22:22:05 -0600

    created function to color code event entries, still buggy.  working on event delegation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 // global var's
+var container = $(".container");
 
 // write function to display current day using moment.js
 function displayDate() {
@@ -14,9 +15,8 @@ displayDate();
 
 // write function which dynamically creates a card,
 // which displays "hour", a textblock, and a button
-function createBlock(string) {
+function createBlock(time) {
   // target class of container from document
-  var container = $(".container");
 
   // using jquery to create elements
   // timeBlock creates div as a row
@@ -24,25 +24,36 @@ function createBlock(string) {
 
   //   column containing event time
   var hourEl = $("<div>").addClass("col-lg-2 hour");
-  var hourlyTime = $("<p>").text(string);
-  hourlyTime.css("font-weight", "bold");
+  var hourlyTime = $("<p>")
+    .text(time)
+    .css("font-weight", "bold")
+    .attr("id", "hour");
+
+  // column containing div with child form
+  var divWithForm = $("<div>").addClass("col-lg-10");
+
+  // form with class row
+  var formEl = $("<form>").addClass("row");
 
   // column containing text area
   var textEl = $("<textarea>")
-    .addClass("col-lg-8 form-control")
-    .attr("id", "text-area");
+    .addClass("col-lg-10 form-control")
+    .attr({ id: "text-area", type: "text", name: "calendar-event" });
 
   // column containing button
-  var saveBtn = $("<button>").addClass("saveBtn col-lg-2");
-  var idiomatic = $("<i>").text("Save");
+  var saveBtn = $("<input>")
+    .attr({"type": "submit", "value": "submit"})
+    .addClass("saveBtn col-lg-2");
+//   var idiomatic = $("<i>").text("Save");
 
   // using jquery to append elements to document
   hourEl.append(hourlyTime);
-  saveBtn.append(idiomatic);
-  timeBlock.append(hourEl, textEl, saveBtn);
+//   saveBtn.append(idiomatic);
+  formEl.append(textEl, saveBtn);
+  divWithForm.append(formEl);
+  timeBlock.append(hourEl, divWithForm);
   container.append(timeBlock);
 }
-
 
 // write function which appends card to document for the hours of
 // 9 - 5 using moment.js
@@ -54,7 +65,7 @@ function appendBlockWithTime() {
   for (i = 0; i <= 8; i++) {
     // numerator increases by 1
     var numerator = (num += 1);
-    var setHour = moment().set("hour", 9);
+    var setHour = moment().set("hour", 13);
     // with each iteratoin setHour increases by numerator
     var addHour = moment(setHour).add(numerator, "h");
     // sets display format
@@ -62,14 +73,52 @@ function appendBlockWithTime() {
 
     //   passing in hour as an argument during function call
     createBlock(displayHour);
+    displayColor(displayHour);
   }
 }
+
 appendBlockWithTime();
+
 // Write function to display each timeblock in past, present, or future
 // colors based on css classes
+function displayColor(hour) {
+  var now = moment().format("h a");
+  var textArea = $("#text-area");
+
+  if (hour < now) {
+    textArea.addClass("past");
+  }
+  // else if (hour === now) {
+  //   textArea.addClass("present");
+  // } else if (hour > now) {
+  //   textArea.addClass("future");
+  // }
+
+  console.log("current hour " + now);
+  console.log("calender hour " + hour);
+
+  console.log(hour < now);
+  // console.log(moment(hour).isBefore(now));
+
+  // console.log(hour === now);
+  // console.log(moment(hour).isSame(now));
+
+  // console.log(hour > now);
+  // console.log(moment(hour).isAfter(now));
+
+  // console.log(moment(hour).isBefore(now));
+  // console.log(!(moment(hour).isBefore(now)));
+  // console.log(moment(hour) === moment(now));
+}
 
 // write function to save event entered upon button click into
 // localstorage
+function sendToLocalStorage(event) {
+  event.preventDefault();
+  var calendarEvent = $('textarea[name="calendar-event"]').val();
+  console.log(calendarEvent);
+}
 
 // using event delegation, create event listener on save button for
 // dynamic content
+container.on("click", ".saveBtn", sendToLocalStorage);
